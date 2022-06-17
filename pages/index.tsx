ee0cec6689d19f7d0c793662c85b9ff81e8918bf
adefope oluwatosin <sirtosin45@gmail.com>
2022-06-17 10:19:59 +0100

    next-template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
 
+import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </Layout>
-)
+);
 
 export default IndexPage
